58d85b0680fa335d30a15eddfb07e96d54e04b2c
Ethan Peterson <epeterson@rsgc.on.ca>
2017-03-17 23:16:22 -0400

    Made a successful POST request using the Alamofire HTTP networking library

--- RSGC Organizer/RSGC Organizer/ViewController.swift
+++ RSGC Organizer/RSGC Organizer/ViewController.swift
@@ -6,21 +6,43 @@
 //  Copyright © 2017 Ethan Peterson. All rights reserved.
 //
 
-import Alamofire
+
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let parameters : [String : Any] = [
+            "username":"ALAMO_FIRE_POST",
+            "password":"FIRE",
+            "first_name":"REMEMBER THE",
+            "last_name":"ALAMO!",
+            "email":"alamo@fire.com"
+        ]
+        Alamofire.request("http://127.0.0.1:8000/register/", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            print(response)
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+//        Alamofire.request("https://ethanpeterson.tech/service.php").responseJSON { response in
+//            print(response.request)  // original URL request
+//            print(response.response) // HTTP URL response
+//            print(response.data)     // server data
+//            print(response.result)   // result of response serialization
+//            
+//            if let JSON = response.result.value {
+//                print("JSON: \(JSON)")
+//            }
+//
+//    }
+
+
     }
-
-
 }
-
