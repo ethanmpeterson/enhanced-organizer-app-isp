3ad39b1cd49066334f617472f33ef7db6a023213
Ethan Peterson <epeterson@rsgc.on.ca>
2017-03-23 22:36:43 -0400

    When the user hits the textfield to change the date, a date picker is shown instead of a keyboard.

--- RSGC Organizer/RSGC Organizer/ScheduleViewController.swift
+++ RSGC Organizer/RSGC Organizer/ScheduleViewController.swift
@@ -9,15 +9,44 @@
 import UIKit
 import Alamofire
 
-class ScheduleViewController: UIViewController {
+class ScheduleViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var dateDisplay: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated:true)
         // Do any additional setup after loading the view.
-        
+        dateDisplay.delegate = self
     }
 
+    // TEXT FIELD AND DATEPICKER CODE:
+    
+    func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let datePicker = UIDatePicker()
+        textField.inputView = datePicker
+        datePicker.datePickerMode = UIDatePickerMode.date
+        datePicker.backgroundColor = UIColor.white
+        datePicker.addTarget(self, action: #selector(ScheduleViewController.datePickerChanged(_:)), for: .valueChanged)
+    }
+    
+    func datePickerChanged(_ sender: UIDatePicker) { // runs when the user selects a new date in the DatePicker
+    
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dateDisplay.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        closeKeyboard()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
